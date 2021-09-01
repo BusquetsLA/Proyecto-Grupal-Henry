886165da60e1c9008a8c9016cc93f47ce6b7aad4
Keltuzad29 <c.cerquera.123@gmail.com>
2021-09-01 00:45:49 -0500

    Creacion de NavBar con CSS y Rutas App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from "./Componentes/Home/Home";
+import About from "./Componentes/About/About";
+import Shop from "./Componentes/Shop/Shop";
+import Help from "./Componentes/Help/Help";
+import Cart from "./Componentes/Cart/Cart";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Hola Mundo</h1>
+    <Switch>
+    <Route path='/' component = {Home}/>
+    <Route path='/Shop' component = {Shop}/>
+    <Route exact path='/Cart' component = {Cart}/>
+    <Route exact path='/About' component = {About}/>
+    <Route exact path= '/Help' component = {Help}/>
+    </Switch>
+      
     </div>
+    </BrowserRouter>
   );
 }
 
