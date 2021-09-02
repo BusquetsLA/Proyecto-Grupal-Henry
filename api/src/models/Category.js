cb360e2e374c11488fbcdd4dabf69490d4535bb6
Adriana Cejas <83995401+adriadria@users.noreply.github.com>
2021-09-01 21:08:11 -0300

    Revert "Server y db"

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Category = new Schema({
-    name: {type:String, required:true},
-    products: [{type: Schema.Types.ObjectId, ref: 'products'}]
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
 
 const Category = mongoose.model('category', categorySchema);
 
--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,45 @@
 const mongoose = require('mongoose');
 
-const Product = new Schema({
-    image_url: {type:String, required:true},
-    name: {type:String, required:true},
-    description: {type:String, required:true},
-    price: {type:Number, required:true},
-    categories: [{type: Schema.Types.ObjectId, ref: 'categories'}]
-});
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: mongoose.Types.Decimal128,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        weight: {
+            type: mongoose.Types.Decimal128,
+            required: true
+        },
+        height: {
+            type: mongoose.Types.Decimal128,
+            required: true
+        },
+        width: {
+            type: mongoose.Types.Decimal128,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true
+        },
+        category: {
+            type: [mongoose.Types.ObjectId]
+        }
+    },
+    { timestamp: true }
+);
 
 const Product = mongoose.model('product', productSchema);
 
