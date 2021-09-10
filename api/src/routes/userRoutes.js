0a26ff080dea855833fd021d6ea6ceb1a2040e94
BusquetsLA <busquetsla@gmail.com>
2021-09-10 16:21:57 -0300

    agregada ruta id

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const { signUp, signIn } = require('../controllers/userControllers');
+const { signUp, signIn, getUserById, editProfile } = require('../controllers/userControllers');
 
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.get('/:id', getUserById);
+router.put('/edit', editUser);
+
 
 module.exports = router;
