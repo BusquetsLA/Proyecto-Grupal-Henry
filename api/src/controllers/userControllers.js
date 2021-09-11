8f610d4bd94fe39df109da1404ad2530b92a8b80
BusquetsLA <busquetsla@gmail.com>
2021-09-11 00:23:48 -0300

    agregados los controladores para el usuario

--- api/src/controllers/userControllers.js
+++ api/src/controllers/userControllers.js
@@ -3,42 +3,23 @@ const bcrypt = require("bcrypt");
 const { generateToken, isAuth } = require("./utils");
 
 async function signUp(req, res, next) {
+  const { name, email, password, isAdmin, susbscribed } = req.body;
   try {
-    // console.log(req.body);
-    const { name, email, password, /* country, phone, address,*/ isAdmin } = req.body;
-    const user = await User.findOne({ email: email });
-    if (user) {
-      if (bcrypt.compareSync(password, user.password)) {
-        return res.status(400).send({ msg: "El usuario ya existe." });
-      }
-      return res.status(400).send({ msg: "Ya existe un usuario registrado con este email. Por favor elige otro.", });
+    const hashedPass = await bcrypt.hashSync(password, 8); // la pass que ingresa el usuario se guarda ya hasheada
+    const [user, created] = await User.findOrCreate({
+      where: { email: email },
+      defaults: {
+        email,
+        name: name,
+        password: hashedPass,
+        isAdmin: isAdmin,
+        susbscribed, // al newsletter
+      },
+    });
+    if (created) { // es un booleano que retorna el findOrCreate
+      return res.status(200).send('Usuario creado con éxito.', { user: user });
     } else {
-      User.create(
-        {
-          name,
-          email,
-          password: bcrypt.hashSync(password, 8),
-          /*country, phone, address,*/
-          // isAdmin, ??
-        },
-        function (err, userCreated) {
-          if (err) {
-            next(err);
-            return res.status(400).send({ msg: "Hubo algun error con los datos proporcionados" });
-          } else {
-            return res.send({
-              msg: "Usuario creado con exito!",
-              data: {
-                _id: userCreated._id,
-                name: userCreated.name,
-                email: userCreated.email,
-                isAdmin: userCreated.isAdmin,
-                token: generateToken(userCreated),
-              },
-            });
-          }
-        }
-      );
+      return res.status(400).send('El usuario ya existe.', { user: user });
     }
   } catch (error) {
     next(error);
@@ -46,23 +27,14 @@ async function signUp(req, res, next) {
 };
 
 async function signIn(req, res, next) {
+  const { email, password } = req.body;
   try {
-    // console.log(req.body);
-    const { email, password } = req.body;
     const user = await User.findOne({ email: email });
     if (user) {
-      bcrypt.compareSync(password, user.password)
-        ? res.send({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user), // hashString que genera jsonwebtoken para autenticar
-          })
-        : res.status(400).send({ msg: "Contraseña incorrecta." });
-    } else {
-      return res.status(400).send({ msg: "Email incorrecto." });
-    }
+      if (bcrypt.compareSync(password, user.password) === true) {
+        return res.status(200).json(user);
+      } else return res.status(400).send('Contraseña incorrecta.');
+    } else return res.status(400).send('Email incorrecto.');
   } catch (error) {
     next(error);
   }
@@ -72,41 +44,57 @@ async function getUserById(req, res, next) {
   const { id } = req.params;
   try {
     const user = await User.findById(id);
+    return res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+async function updateUserById(req, res, next) {
+  const { id } = req.params;
+  const { name, email, password, isAdmin, susbscribed } = req.body;
+  try {
+    const user = await User.findById(id);
     if (user) {
-      return res.send(user);
+      await User.updateOne({_id: id}, { name, email, password, isAdmin, susbscribed });
+      return res.status(200).send('Usuario actualizado correctamente.');
     } else {
-      return res.status(400).send({ msg: "Usuario no encontrado." });
+      return res.status(400).send('Usuario no encontrado.');
     }
   } catch (error) {
     next(error);
   }
 };
-/*
-async function editUser(req, res, next) {
+
+async function deleteUser(req, res, next) {
+  const { id } = req.params;
   try {
-    const user = await User.findById(re.user._id);
+    const user = await User.findById(id);
     if (user) {
-      user.name = req.name || user.name;
-      user.email = req.email || user.email;
+      await User.deleteOne({ _id: id });
+      return res.status(200).send('Usuario eliminado.');
+    } else {
+      return res.status(400).send('Usuario no encontrado');
     }
-    if (req.password) {
-      user.password = bcrypt.hashSync(req.password, 8);
-    }
-    return res.send({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-      token: generateToken(updatedUser),
-    });
   } catch (error) {
     next(error);
   }
-}*/
+};
+
+async function getUsers(req, res, next) {
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   signUp,
   signIn,
   getUserById,
-  /*editUser,*/
+  updateUserById,
+  deleteUser,
+  getUsers,
 };
