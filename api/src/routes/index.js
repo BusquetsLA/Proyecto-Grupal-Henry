9a89f6de2456cc343dcaa6e0172824548fd24fef
Keltuzad29 <c.cerquera.123@gmail.com>
2021-09-12 14:21:25 -0500

    se agregan nuevos productos y funcionalidades de envio de correo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const productRouter = require('./product.js');
 const categoryRouter = require('./category.js');
 const userRouter = require('./userRoutes');
 const reviewRouter = require('./review.js');
+const sendMailRouter = require('./sendMail.js');
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/products', productRouter);
 router.use('/categories', categoryRouter);
 router.use('/user', userRouter);
 router.use('/reviews', reviewRouter);
+router.use('/sendMail', sendMailRouter);
 
 module.exports =  router;
