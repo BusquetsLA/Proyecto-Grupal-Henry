c7c065f925e3067acd22241bd8e3967ebbbfd06f
Alejandro Flores <sjafs.83@gmail.com>
2021-09-12 16:20:14 -0300

    configuracion archivos para deploy de backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 
+const other = require('./other');
 const productRouter = require('./product.js');
 const categoryRouter = require('./category.js');
 const userRouter = require('./userRoutes');
@@ -7,6 +8,7 @@ const reviewRouter = require('./review.js');
 
 const router = Router();
 
+router.use('/',other);
 router.use('/products', productRouter);
 router.use('/categories', categoryRouter);
 router.use('/user', userRouter);
