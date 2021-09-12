f4bdff096e4c7f38ba562753b63de583dc1788ce
Keltuzad29 <c.cerquera.123@gmail.com>
2021-09-12 17:13:34 -0500

    se modifica formulario help y se conecta con actions

--- client/src/components/Help/Help.jsx
+++ client/src/components/Help/Help.jsx
@@ -1,30 +1,44 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink, useHistory } from "react-router-dom";
 import styles from "./Help.module.css";
+import { sendEmail } from '../../redux/actions';
+
 
 const Help = () => {
-  //    const $form = document.querySelector('#form')
-  //    const $buttonMailto = document.querySelector("#oculto")
-  //   $form.addEventListener('submit', handleSubmit);
+  const dispatch = useDispatch();
+  const history = useHistory()
+  const[input, setInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+})
 
-  //    function handleSubmit(e) {
-  //             e.preventDefault();
-  //     const form = new FormData(this);
-  //     console.log(form.get("name"));
-  //     $buttonMailto.serAttribute(
-  //       'href',
-  //       `mailto:estilopropio@gmail.com?subject=nombre ${form.get(
-  //         "name"
-  //       )} correo ${form.get("email")}&body=${form.get("message")}`
-  //     );
-  //     $buttonMailto.click();
-  //    }
+function handleChange(e){
+
+  setInput({
+  ...input,
+  [e.target.name] : e.target.value
+  })
+  console.log(input) //muestra el input en el navegador
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  console.log(input)
+  dispatch(sendEmail(input))
+  setInput({
+      name: "", email: "", message: "",})
+  history.push('/')
+}
+  
+
   return (
     <div>
       <NavLink className={styles.link_text} to="/">
         Inicio
       </NavLink>
-      <form id="form" action="/sendEmail" method="POST" className={styles.form}>
+       <form id="form"  className={styles.form} onSubmit={(e)=>handleSubmit(e)}> {/* action="/sendEmail" method="POST" */}
         <h3 className={styles.tag}>
           <span>SI TIENES ALGUNA DUDA O SUGERENCIA ENVIANOS UN CORREO</span>
         </h3>
@@ -37,7 +51,7 @@ const Help = () => {
           type="text"
           id="name"
           placeholder="Dejanos Tu Nombre"
-        />
+          onChange={(e)=>handleChange(e)}/>
         <label for="email">
           Correo electrónico <span>*</span>
         </label>
@@ -47,9 +61,9 @@ const Help = () => {
           id="email"
           required
           placeholder="Correo de Contacto"
-        />
+          onChange={(e)=>handleChange(e)}/>
         <label for="message">Mensaje</label>
-        <textarea id="message" name="message" cols="30" rows="9"></textarea>
+        <textarea id="message" name="message" cols="30" rows="9" onChange={(e)=>handleChange(e)}></textarea>
         <button type="submit" className={styles.btn}>
           enviar mensaje
         </button>
