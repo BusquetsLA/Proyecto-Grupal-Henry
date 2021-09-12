d23a9afea42d04702c3f657e9582f982299ecd7c
Alejandro Flores <sjafs.83@gmail.com>
2021-09-12 17:17:51 -0300

    deploy

--- api/src/routes/other.js
+++ api/src/routes/other.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send(`<h1>Hello World</h1> this is the Backend for E-comerce App of FT-15a Group 10, develop by: </br>
+    res.send(`<h1>Hello World</h1> This is the Backend for E-comerce App of FT-15a Group 10, develop by: </br>
      - Camilo Cerquera</br>
      - Esteban Choque</br>
      - Facundo Figueroa</br>
