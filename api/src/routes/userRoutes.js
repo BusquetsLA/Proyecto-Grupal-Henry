901f3a2e8346f92621da876a3777f9381a538b22
BusquetsLA <busquetsla@gmail.com>
2021-09-20 13:43:45 -0300

    agregadas las rutas forgot y reset del usuario

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
-const { signUp, signIn, getUserById, updateUserById, deleteUser, getUsers, signInFirebase, updateCart } = require('../controllers/userControllers');
+const { signUp, signIn, getUserById, updateUserById, deleteUser, getUsers, signInFirebase, updateCart, passwordForgot, passwordReset } = require('../controllers/userControllers');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/signinfirebase', signInFirebase);
+router.post('/forgot', passwordForgot);
 router.put('/update/:id', updateUserById);
+router.put('/reset/:id', passwordReset);
 router.post('/updateCart/:id', updateCart)
 router.delete('/delete/:id', deleteUser);
+
 
 module.exports = router;
