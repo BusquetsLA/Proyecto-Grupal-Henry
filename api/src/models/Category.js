4a4159043107be339ef843c022324bef6729f3df
Alejandro Flores <sjafs.83@gmail.com>
2021-09-21 04:48:56 -0300

    revision conexion usuarios

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Category = new Schema({
-    name: {type:String, required:true},
-    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'products'}]
+    name:    {type:String, required:true},
+    products:[{type: mongoose.Schema.Types.ObjectId, ref: 'products'}]
 });
 
 
--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,15 +2,23 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "review" }],
-    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
-    subscribed: { type: Boolean, default: false },
-    blocked: { type: Boolean, default: false },
-    logged: { type: Boolean, default: false },
+    name:      { type: String, required: true },
+    email:     { type: String, required: true, unique: true, lowercase: true },
+    password:  { type: String, required: true },
+    isAdmin:   { type: Boolean, required: true, default: false },
+    reviews:   [{ type: mongoose.Schema.Types.ObjectId, ref: "review" }],
+    orders:    [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+    subscribed:{ type: Boolean, default: false },
+    blocked:   { type: Boolean, default: false },
+    logged:    { type: Boolean, default: false },
+    typelogin: { 
+      type: String, 
+      enum: {
+        values: ['Normal', 'Google', 'Facebook'],
+        message: '{VALUE} is not supported'
+      },
+      default: 'Normal',
+      required: false },
     cart: [
       {
         _id: { type: mongoose.Schema.Types.ObjectId, ref: "products" },
