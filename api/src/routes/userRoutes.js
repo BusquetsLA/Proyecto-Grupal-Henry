dd615666e6786836263bc999d20ea7e3d2574d6f
BusquetsLA <busquetsla@gmail.com>
2021-09-22 13:40:44 -0300

    user routes para pass reset

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { signUp, signIn, getUserById, updateUserById, deleteUser, getUsers, signInFirebase, updateCart, passwordForgot, passwordReset } = require('../controllers/userControllers');
+const { signUp, signIn, getUserById, updateUserById, deleteUser, getUsers, signInFirebase, updateCart, passwordForgot, passwordData, passwordReset } = require('../controllers/userControllers');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
@@ -8,9 +8,10 @@ router.post('/signin', signIn);
 router.post('/signinfirebase', signInFirebase);
 router.post('/forgot', passwordForgot);
 router.put('/update/:id', updateUserById);
-router.put('/reset/:id', passwordReset);
-router.post('/updateCart/:id', updateCart)
+router.put('/reset', passwordReset);
+router.post('/updateCart/:id', updateCart);
 router.delete('/delete/:id', deleteUser);
+router.get('/reset/:id/:token', passwordData);
 
 
 module.exports = router;
