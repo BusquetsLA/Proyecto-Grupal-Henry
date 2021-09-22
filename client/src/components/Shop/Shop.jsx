4caf3c54e21fefbcc0c434f3372fdbea2bd397b4
Kelduzad29 <c.cerquera.123@gmail.com>
2021-09-21 19:54:12 -0500

    se corrige envio de mail

--- client/src/components/Shop/Shop.jsx
+++ client/src/components/Shop/Shop.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Shop.module.css';
+import Products from "../Products/Products";
+import NavBar from "../NavBar/NavBar";
 
 const Shop = () => {
+  const [order, setOrder] = React.useState("");
+  console.log('order:',order)
     return (
         <div>
             <NavLink
@@ -11,6 +15,8 @@ const Shop = () => {
             >
               Inicio
             </NavLink>
+            <NavBar setOrder={setOrder} />
+            <Products />
         </div>
     )
 }
